c197de52618af8ab15df372e1a1162364143f15b
Lucas.R <45816433+RamisL@users.noreply.github.com>
2020-05-04 11:30:31 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Set your publishable key: remember to change this to your live publishable key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-var stripe = Stripe('pk_test_tb3VmhJ9Q6uMhr7QdrXtIjMP00q0X2NFZo');
-var elements = stripe.elements();
